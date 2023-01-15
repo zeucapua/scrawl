e6c3a2a65a75355977304ca0aca28bf0ee39217b
Zeu Capua <zeucapua@gmail.com>
2023-01-14 22:54:10 -0800

    init commit, textarea + preview divs, css but tailwind is installed, installed svelte-markdown

---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  let writing = ``; 
+</script>
+
+
+<main>
+  <textarea bind:value={writing} />
+  <div id="preview">
+
+  </div>
+</main>
+
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    padding: 3vw;
+    background-color: #171717;
+  }
+
+  textarea {
+    width: 50%;
+    height: 100%;
+    padding: 1rem;
+    color: white;
+    font-size: 20pt;
+    background-color: #404040;
+    border-radius: 0.5rem;
+  }
+
+  #preview {
+    width: 50%;
+    height: 100%;
+    padding: 1rem;
+    color: white;
+  }
+</style>
