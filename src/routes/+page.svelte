4989df6f4678f8d5bbdac4f3462d23cafd596c7c
Zeu Capua <zeucapua@gmail.com>
2023-01-31 17:18:00 -0800

    created Writing type, implemented saving/overwriting writings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,48 @@
   import { writings } from "$lib/stores";
   import Markdown from "svelte-exmarkdown";
 
-  let writing = ``; 
+  let id = "";
+  let content = ""; 
   let name = "";
 
-  function setWriting(w : Record<string, string>) {
+  interface Writing {
+    id : string,
+    name : string,
+    content : string,
+  }
+
+  function newWriting() {
+    id = crypto.randomUUID();
+    name = "New Writing";
+    content = "";
+  }
+
+  function setWriting(w : Writing) {
     // TODO:  if current writing is not saved, give option to saveWriting()
-    writing = w.writing;
+    id = w.id;
     name = w.name;
+    content = w.content;
   }
 
   function saveWriting() {
-    const current_writing = JSON.stringify({ name: name, writing: writing });
-    // TODO: overwrite if name is the same
-    writings.set([...$writings, current_writing]);
+    id = id.length === 0 ? crypto.randomUUID() : id;
+    const current_writing = { id: id, name: name, content: content };
+    if (checkWriting(current_writing)) {
+      const others = list.filter((w : Writing) => { return w.id != current_writing.id });
+      let converted : string[] = [];
+      for (const saved of others) { converted = [...converted, JSON.stringify(saved)]; }
+      writings.set([...converted, JSON.stringify(current_writing)]);
+    }
+    else {
+      writings.set([...$writings, JSON.stringify(current_writing)]);
+    }
+  }
+
+  function checkWriting(w : Writing) {
+    for (const saved of list) {
+      if (saved.id === w.id) { return true; } 
+    }
+    return false;
   }
 
   function clearWritings() {
@@ -22,7 +51,6 @@
   }
 
   $: list = JSON.parse("[" + $writings + "]")
-
 </script>
 
 <main class="flex flex-row p-8 gap-8 w-screen h-screen justify-center bg-neutral-800">
@@ -31,14 +59,21 @@
     <div class="flex flex-row gap-8 justify-center">
       <button on:click={saveWriting} class="w-fit mx-auto px-4 py-2 bg-white text-black">Save</button>
       <button on:click={clearWritings} class="w-fit mx-auto px-4 py-2 bg-white text-black">Clear</button>
+      <button on:click={newWriting} class="w-fit mx-auto px-4 py-2 bg-white text-black">New</button>
     </div>
-    {#each list as w}
-      <button on:click={() => setWriting(w)} class="w-fit mx-auto px-4 py-2 bg-white text-black">{w.name}</button>
-    {/each}
+    <div class="flex flex-col gap-8 w-full">
+      {#each list as w}
+        {#if id === w.id}
+          <button on:click={() => setWriting(w)} class="w-fit px-4 py-2 bg-white text-black">{w.name}</button>
+        {:else}
+          <button on:click={() => setWriting(w)} class="w-fit px-4 py-2 bg-neutral-600 text-white">{w.name}</button>
+        {/if}
+      {/each}
+    </div>
   </div>
-  <textarea class="text-white bg-neutral-600 p-8 w-1/2 rounded-lg" bind:value={writing} />
+  <textarea class="text-white bg-neutral-600 p-8 w-1/2 rounded-lg" bind:value={content} />
   <div class="prose prose-invert overflow-auto p-8 w-1/2 h-full bg-neutral-900">
-    <Markdown md={writing} />
+    <Markdown md={content} />
   </div>
 </main>
 
